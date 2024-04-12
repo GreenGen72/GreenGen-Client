38d0896c8563f61ea71d3a4877a655abc5c571d9
Dorivania Santos <dorim.dev@gmail.com>
2024-04-12 16:44:08 -0300

    :sparkles: adicionado pages, aboutUs e contact.

---
+++ src/pages/aboutUs/AboutUs.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const AboutUs: React.FC = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-2xl font-bold mb-4">Sobre Nós</h2>
+      <p>
+        Bem-vindo à Green Gen, onde a inovação encontra a sustentabilidade para
+        um futuro mais brilhante. Somos uma equipe apaixonada dedicada a
+        promover uma mudança positiva por meio do poder do sol.
+      </p>
+      <p>
+        Na Green Gen, acreditamos que o futuro da energia está na luz do sol,
+        uma fonte inesgotável e renovável de poder. Nossa missão é oferecer uma
+        ampla gama de produtos inovadores que funcionam com energia solar,
+        proporcionando não apenas soluções práticas para o seu dia a dia, mas
+        também ajudando a construir um mundo mais limpo e sustentável para as
+        gerações futuras.
+      </p>
+      <p>
+        Cada produto em nossa loja é cuidadosamente selecionado, garantindo
+        qualidade, eficiência e desempenho excepcionais. Desde iluminação
+        externa e interna até gadgets e dispositivos de energia renovável, temos
+        tudo o que você precisa para aproveitar ao máximo o poder do sol em sua
+        vida diária.
+      </p>
+      <p>
+        Além de oferecer produtos de alta qualidade, estamos empenhados em
+        educar e inspirar nossa comunidade sobre os benefícios da energia solar
+        e práticas sustentáveis. Acreditamos que, juntos, podemos fazer uma
+        diferença significativa na proteção do nosso planeta para as gerações
+        futuras.
+      </p>
+      <p>
+        Junte-se a nós nesta jornada rumo a um futuro mais verde e brilhante. Na
+        Green Gen, estamos iluminando o caminho para um amanhã mais sustentável.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
