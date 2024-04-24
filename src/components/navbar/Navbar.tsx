4239f9a7a8b2691f2d4a1f21772320f809ec5cad
Gabriel Rodrigues <gabers357@gmail.com>
2024-04-24 11:19:05 -0300

    🐛 🔧 Corrigir erros de digitação, formatação no código e rotas

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
-  let navigate = useNavigate();
+  const navigate = useNavigate();
 
   const { usuario, handleLogout } = useContext(AuthContext);
 
@@ -13,44 +13,64 @@ function Navbar() {
     navigate("/login");
   }
 
-  let navbarComponent;
-
-  if (usuario.token !== "") {
-    navbarComponent = (
-      <div className="w-full bg-indigo-900 text-white flex justify-center py-4">
-        <div className="container flex justify-between text-lg">
-          <Link to="/home" className="text-2xl font-bold uppercase">
-            GreenGen
-          </Link>
-
-          <div className="flex gap-4">
-            <Link to="/home" className="hover:underline">
-              Home
-            </Link>
-            <Link to="/categoria" className="hover:underline">
-              Categoria
-            </Link>
-            <Link to="/cadastrar" className="hover:underline">
-              Cadastrar categoria
+  return (
+    <>
+      {usuario.token !== "" ? (
+        <div className="w-full bg-indigo-900 text-white flex justify-center py-4">
+          <div className="container flex justify-between text-lg">
+            <Link to="/home" className="text-2xl font-bold uppercase">
+              GreenGen
             </Link>
 
-            <Link to="/about" className="hover:underline">
-              Sobre nós
-            </Link>
+            <div className="flex gap-4">
+              <Link to="/home" className="hover:underline">
+                Home
+              </Link>
+              <Link to="/categoria" className="hover:underline">
+                Categoria
+              </Link>
+              <Link to="/cadastrar" className="hover:underline">
+                Cadastrar categoria
+              </Link>
 
-            <Link to="/contato" className="hover:underline">
-              Contato
-            </Link>
+              <Link to="/about" className="hover:underline">
+                Sobre nós
+              </Link>
 
-            <Link to="" onClick={logout} className="hover:underline">
-              Sair
-            </Link>
+              <Link to="/contato" className="hover:underline">
+                Contato
+              </Link>
+
+              <Link to="" onClick={logout} className="hover:underline">
+                Sair
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-    );
-  }
-  return <>{navbarComponent}</>;
+      ) : (
+        <div className="w-full bg-indigo-900 text-white flex justify-center py-4">
+          <div className="container flex justify-between text-lg">
+            <Link to="/home" className="text-2xl font-bold uppercase">
+              GreenGen
+            </Link>
+
+            <div className="flex gap-4">
+              <Link to="/about" className="hover:underline">
+                Sobre nós
+              </Link>
+
+              <Link to="/contato" className="hover:underline">
+                Contato
+              </Link>
+              <Link to="/login" className="hover:underline">
+                Login
+              </Link>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default Navbar;
