af2b3c61314cb162f1015899c642306c14168259
Gabriel Rodrigues <gabers357@gmail.com>
2024-04-25 18:56:03 -0300

    🔧 Corrige bug no componente Footer: ajusta renderização condicional do conteúdo

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,36 +3,36 @@ import { useContext } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
 
 function Footer() {
+  const { usuario } = useContext(AuthContext);
 
-    const { usuario, handleLogout } = useContext(AuthContext);
+  return usuario.token !== "" ? (
+    <div className="bg-indigo-900 text-white">
+      <div className="container flex flex-col items-center py-4">
+        <p className="text-xl font-bold">
+          Green Gen 72 | © {new Date().getFullYear()}
+        </p>
+        <p className="text-lg">Acesse nossas redes sociais:</p>
+        <div className="flex gap-2">
+          <a
+            href="https://www.instagram.com/greengen_72?igsh=MTdjNDU1Z2hkd2libg=="
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <InstagramLogo size={48} weight="bold" alt="Instagram" />
+          </a>
 
-    let footerComponent;
-
-    if (usuario.token !== "") {
-        footerComponent = (
-        <>
-            <div className="bg-indigo-900 text-white">
-                <div className="container flex flex-col items-center py-4">
-                    <p className='text-xl font-bold'>Green Gen 72 | © {new Date().getFullYear()}</p>
-                    <p className='text-lg'>Acesse nossas redes sociais:</p>
-                    <div className='flex gap-2'>
-
-                        <a href="https://www.instagram.com/greengen_72?igsh=MTdjNDU1Z2hkd2libg==" target="_blank" rel="noopener noreferrer">
-                            <InstagramLogo size={48} weight='bold' alt="Instagram" />
-                        </a>
-
-                        <a href="https://github.com/GreenGen72" target="_blank" rel="noopener noreferrer">
-                            <GithubLogo size={48} weight='bold' alt="GitHub" />
-                        </a>
-                    </div>
-                </div>
-            </div>
-
-        </>
-    );
-}
-
-return <>{footerComponent}</>;
-
+          <a
+            href="https://github.com/GreenGen72"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <GithubLogo size={48} weight="bold" alt="GitHub" />
+          </a>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <></>
+  );
 }
 export default Footer;
