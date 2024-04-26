bef0dc14a1e1fc4fe20f12583e0fba5fa06daa1c
Gabriel Rodrigues <gabers357@gmail.com>
2024-04-26 14:48:41 -0300

    🔧 🐛 Corrige a função handleRemoveToCart em CartContext.tsx

--- src/components/produtos/cardProdutos/CardProdutos.tsx
+++ src/components/produtos/cardProdutos/CardProdutos.tsx
@@ -1,11 +1,20 @@
 import { Link } from "react-router-dom";
 import Produto from "../../../models/Produto";
+import { useContext } from "react";
+import { AuthContext } from "../../../contexts/AuthContext";
+import { CartContext } from "../../../contexts/CartContext";
 
 interface CardProdutoProps {
   produto: Produto;
 }
-
 function CardProduto({ produto }: CardProdutoProps) {
+  const { usuario } = useContext(AuthContext);
+  console.log('usuario: ', usuario);
+  const isAdmin = true;
+  // const isAdmin = usuario.admin;
+
+  const { handleAddToCart, produtosNoCarrinho } = useContext(CartContext);
+  console.log("produtosNoCarrinho: ", produtosNoCarrinho);
   return (
     <div className="border flex flex-col rounded-2xl overflow-hidden justify-between">
       <header className="py-2 px-6 bg-indigo-800 text-white font-bold text-2xl">
@@ -13,25 +22,34 @@ function CardProduto({ produto }: CardProdutoProps) {
       </header>
       <p className="p-8 text-3xl bg-slate-200 h-full">{produto.nome}</p>
       <p className="p-8 text-3xl bg-slate-200 h-full">{produto.descricao}</p>
-      <p className="p-8 text-3xl bg-slate-200 h-full">{produto.preco}</p>
+      <p className="p-8 text-3xl bg-slate-200 h-full text-red-700">
+        {produto.preco}
+      </p>
       <p className="p-8 text-3xl bg-slate-200 h-full">{produto.quantidade}</p>
       <p className="p-8 text-3xl bg-slate-200 h-full">
         {produto.categoria?.descricao}
       </p>
 
+      {isAdmin ?(
+        <div className="flex">
+          <Link
+            to={`/editarProduto/${produto.id}`}
+            className="w-full text-slate-100 bg-indigo-400 hover:bg-indigo-800 flex items-center justify-center py-2"
+          >
+            <button>Editar</button>
+          </Link>
+          <Link
+            to={`/deletar-produto/${produto.id}`}
+            className="text-slate-100 bg-red-400 hover:bg-red-700 w-full flex items-center justify-center"
+          >
+            <button className="w-full text-slate-100 bg-indigo-400 hover:bg-indigo-800 flex items-center justify-center py-2">
+              Deletar
+            </button>
+          </Link>
+        </div>
+      )}
       <div className="flex">
-        <Link
-          to={`/editarProduto/${produto.id}`}
-          className="w-full text-slate-100 bg-indigo-400 hover:bg-indigo-800 flex items-center justify-center py-2"
-        >
-          <button>Editar</button>
-        </Link>
-        <Link
-          to={`/deletar-produto/${produto.id}`}
-          className="text-slate-100 bg-red-400 hover:bg-red-700 w-full flex items-center justify-center"
-        >
-          <button>Deletar</button>
-        </Link>
+        <button onClick={() => handleAddToCart(produto)}>Comprar </button>
       </div>
     </div>
   );
--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -10,6 +10,8 @@ import Produto from "../models/Produto";
 interface CartContextProps {
   produtosNoCarrinho: Produto[];
   setProdutosNoCarrinho: Dispatch<SetStateAction<Produto[]>>;
+  handleAddToCart: (produto: Produto) => void;
+  handleRemoveToCart: (produto: Produto) => void;
 }
 
 export const CartContext = createContext<CartContextProps>(
@@ -20,14 +22,42 @@ interface CartProviderProps {
   children: ReactNode;
 }
 export function CartProvider({ children }: CartProviderProps) {
-  const [produtosNoCarrinho, setProdutosNoCarrinho] = useState<Produto[]>(
-    [] as Produto[]
-  );
-
+  const [produtosNoCarrinho, setProdutosNoCarrinho] = useState<Produto[]>([
+    {
+      id: 1,
+      nome: "Produto Exemplo",
+      descricao: "Descrição do Produto Exemplo",
+      preco: 10.0,
+      quantidade: 1,
+      categoria: null,
+      usuario: null,
+    },
+    {
+      id: 2,
+      nome: "Segundo Produto Exemplo",
+      descricao: "Descrição do Produto Exemplo",
+      preco: 18.0,
+      quantidade: 1,
+      categoria: null,
+      usuario: null,
+    },
+  ]);
+  const handleAddToCart = (produto: Produto) => {
+    setProdutosNoCarrinho([...produtosNoCarrinho, produto]);
+    console.log("add to the cart");
+  };
+  const handleRemoveToCart = (produto: Produto) => {
+    // setProdutosNoCarrinho(() => {
+    //   produtosNoCarrinho.filter((produto) => produto.id !== produto.id);
+    // });
+    console.log(`${produto}`);
+  };
   return (
     <CartContext.Provider
       value={{
+        handleAddToCart,
         produtosNoCarrinho,
+        handleRemoveToCart,
         setProdutosNoCarrinho,
       }}
     >
