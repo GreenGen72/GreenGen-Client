18285280afd6db6f5281c2dd8131accef38655fd
Dorivania Santos <dorim.dev@gmail.com>
2024-04-27 22:08:08 -0300

    :recycle: refatorando

--- src/components/categoria/cardCategoria/CardCategoria.tsx
+++ src/components/categoria/cardCategoria/CardCategoria.tsx
@@ -7,13 +7,13 @@ interface CardCategoriaProps {
 
 function CardCategoria({ categoria }: CardCategoriaProps) {
   return (
-    <div className="border flex flex-col rounded-2xl overflow-hidden justify-between">
+    <div className="border flex flex-col flex-grow rounded-2xl overflow-hidden justify-between">
       <header className="py-2 px-6 bg-indigo-800 text-white font-bold text-2xl">
         Categoria
       </header>
       <p className="p-8 text-3xl bg-slate-200 h-full">{categoria.nome}</p>
       <p className="p-8 text-3xl bg-slate-200 h-full">{categoria.descricao}</p>
-      <div className="flex">
+      <div className="flex flex-grow">
         <Link
           to={`/editar-categoria/${categoria.id}`}
           className="w-full text-slate-100 bg-indigo-400 hover:bg-indigo-800 flex items-center justify-center py-2"
