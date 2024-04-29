51aabb22663252d5a56fd5988acb4af2d733f316
Dorivania Santos <dorim.dev@gmail.com>
2024-04-28 23:29:17 -0300

    adiciona busca

--- src/components/produtos/cardProdutos/CardProdutos.tsx
+++ src/components/produtos/cardProdutos/CardProdutos.tsx
@@ -1,31 +1,35 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { CartContext } from "../../../contexts/CartContext";
 import { AuthContext } from "../../../contexts/AuthContext.tsx";
 import Produto from "../../../models/Produto.ts";
+import { toastAlerta } from "../../../utils/toastAlerta.ts";
 
 interface CardProdutoProps {
   produto: Produto;
 }
 
 function CardProduto({ produto }: CardProdutoProps) {
-  const { isAdmin } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const { usuario, isAdmin } = useContext(AuthContext);
 
-  // const isAdmin = usuario.admin;
   const { adicionaProdutoNoCarrinho } = useContext(CartContext);
+
+  const handleComprarClick = () => {
+    if(usuario.nome == ""){
+      toastAlerta("Logue para comprar", "info");
+      navigate("/login");
+    }else
+
+    adicionaProdutoNoCarrinho(produto);
+   
+  };
 
   return (
     <div className="border flex flex-col flex-grow overflow-hidden place-content-centershadow-xl w-4/5 mx-12 h-full content-center justify-center ">
-      {/*<header className="py-2 px-6 bg-indigo-800 text-white font-bold text-2xl " >*/}
-      {/*  Produto*/}
-      {/*</header>*/}
       <img src={produto.descricao} className="w-fit h-fit p-2" alt={"xxxx"} />
       <p className="p-2">{produto.nome}</p>
       <p className="p-2">{produto.preco}</p>
-      {/* <p className="p-2">*/}
-      {/*      {produto.categoria?.descricao}*/}
-      {/*  </p>*/}
-
       {isAdmin ? (
         <div className="flex">
           <Link
@@ -46,7 +50,7 @@ function CardProduto({ produto }: CardProdutoProps) {
       ) : (
         <div className="flex">
           <button
-            onClick={() => adicionaProdutoNoCarrinho(produto)}
+            onClick={handleComprarClick}
             className="w-full text-slate-100 bg-main-green hover:bg-main-light-green flex items-center justify-center py-2"
           >
             Comprar
