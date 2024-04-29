9121f681869e0b76884ebc06fedbf9a0fc21f33f
devgabrielnascimento <devgabrielnascimento@gmail.com>
2024-04-28 22:31:53 -0300

    :bug: corrigindo problemas no navbar, sobre etc

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -24,18 +24,18 @@ function Navbar() {
 
   const renderLoggedInNavbar = () => (
     <nav className="grid grid-cols-1 bg-main-green justify-center items-center text-white">
-      <div className="flex justify-start items-center gap-10 py-4 px-10">
-        <Link to="/home" className="w-auto justify-center items-center">
+      <div className="flex items-center  py-4 px-10">
+        <Link to="/home" className="flex w-1/3 justify-start items-center">
           <img src={LogoGreenGen} alt="Logo Green Gen" />
         </Link>
-        <div className="flex mx-auto text-xl">
+        <div className="flex mx-auto text-xl w-1/3">
           <input
             type="text"
-            className="flex bg-white placeholder-gray-400 text-gray-900 p-2 w-full"
+            className="flex placeholder-gray-400 text-gray-900 p-6 w-full h-16"
             placeholder="Pesquise seu produto aqui"
           />
           <button
-            className="bg-main-light-green ml-2 p-2 rounded-full"
+            className="bg-main-light-green ml-2 p-2 rounded-full w-20"
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
           >
@@ -45,13 +45,14 @@ function Navbar() {
             />
           </button>
         </div>
-        <div className="flex gap-2 px-4 justify-center items-center hover:bg-main-light-green rounded-full">
+        <div className="flex gap-2 px-4 w-1/3 justify-end items-center">
+          <div className="flex px-4 gap-2 hover:bg-main-light-green rounded-full">
           <img
             src={usuario.foto}
             className="w-12 h-12 rounded-full"
             alt="foto de perfil"
           />
-          <ul className="">
+          <ul className="grid items-center">
             <li>Olá, {usuario.nome.split(" ")[0]}!</li>
           </ul>
         </div>
@@ -62,6 +63,9 @@ function Navbar() {
           <img src={exitIconNB} alt="Ícone de saída" />
         </Link>
       </div>
+      
+          </div>
+          
       <nav className="flex flex-row justify-center bg-main-light-green text-center w-full items-center gap-10">
         <div className="flex tracking-widest gap-10 justify-center">
           <div className="dropdown dropdown-hover hover:bg-main-base-color hover:text-main-green">
@@ -139,18 +143,18 @@ function Navbar() {
 
   const renderLoggedOutNavbar = () => (
     <nav className="grid grid-cols-1 bg-main-green justify-center items-center text-white">
-      <div className="flex justify-start items-center gap-10 py-4 px-10">
-        <Link to="/home" className="w-auto justify-center items-center">
+      <div className="flex  items-center  py-4 px-10">
+        <Link to="/home" className="flex w-1/3 justify-start items-center">
           <img src={LogoGreenGen} alt="Logo Green Gen" />
         </Link>
-        <div className="flex mx-auto text-xl">
+        <div className="flex mx-auto text-xl w-1/3">
           <input
             type="text"
-            className="flex bg-white placeholder-gray-400 text-black p-2 w-full"
+            className="flex  placeholder-gray-400 text-gray-900 p-6 w-full h-16"
             placeholder="Pesquise seu produto aqui"
           />
           <button
-            className="bg-main-light-green ml-2 p-2 rounded-full"
+            className="bg-main-light-green ml-2 p-2 rounded-full w-20"
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
           >
@@ -160,7 +164,8 @@ function Navbar() {
             />
           </button>
         </div>
-        <div className="flex gap-2 px-4 hover:bg-main-light-green rounded-full">
+        <div className="flex gap-2 px-4 w-1/3 justify-end">
+          <div className="flex px-4 gap-2 hover:bg-main-light-green rounded-full">
           <img src={userIconNB} alt="Ícone usuário padrão" />
           <ul className="">
             <li>
@@ -183,7 +188,8 @@ function Navbar() {
               </Link>
             </li>
           </ul>
-        </div>
+          </div>
+        </div> 
       </div>
       <nav className="flex flex-row justify-end bg-main-light-green text-center w-full items-center gap-10">
         <div className="flex tracking-widest gap-10 basis-2/4 justify-center">
@@ -192,7 +198,6 @@ function Navbar() {
               tabIndex={0}
               className="flex items-center justify-center h-10 px-2"
             >
-              
               <Link
                 to="/categoria"
                 className="hover:not-underline px-2 uppercase hover:bg-main-base-color flex items-center justify-center h-10 hover:text-main-green hover:transition-all"
