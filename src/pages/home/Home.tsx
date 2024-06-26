729673a7b07f6230b4b7e1c3e3d02d89033ce2d5
Jonathan Garuti <jonathan_garuti@hotmail.com>
2024-06-26 15:39:23 -0300

    Adicionados banners à tela inicial

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,18 +1,17 @@
 import React from "react";
-//import { AuthContext } from "../../contexts/AuthContext";
-import "./Home.css";
+import { Link } from "react-router-dom";
 import ListaProdutos from "../../components/produtos/listaProdutos/ListaProdutos";
 import { faCreditCard, faTruck } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import PixIcon from "../../assets/Pix.tsx";
+import Carrossel from "./carrossel/carrossel.tsx";
 
 const Home: React.FC = () => {
-  //const { isAdmin } = useContext(AuthContext);
   return (
     <>
-      <section className="bg-white  flex items-center justify-center">
-        <div className="grid items-center px-6 py-6">
-          <section className="flex  items-center gap-60 ">
+      <section className="bg-white flex flex-col items-center justify-center">
+        <div className="w-full max-w-6xl px-6 py-6">
+          <section className="flex justify-center items-center gap-60 mb-8">
             <div className="flex content-center justify-center items-center ">
               <FontAwesomeIcon
                 icon={faTruck}
@@ -20,8 +19,8 @@ const Home: React.FC = () => {
                 size="2x"
               />
               <span className="text-primary">
-                <p className="font-black text-xl">Entrega Flash </p><p>em até 24h</p>
-                 
+                <p className="font-black text-xl">Entrega Flash </p>
+                <p>em até 24h</p>
               </span>
             </div>
             <div className="flex items-center justify-center">
@@ -31,27 +30,29 @@ const Home: React.FC = () => {
                 size="2x"
               />
               <span className="text-primary">
-               <p className="font-black text-xl">Parcele em até 12x</p><p> no cartão de crédito</p>
+                <p className="font-black text-xl">Parcele em até 12x</p>
+                <p> no cartão de crédito</p>
               </span>
             </div>
             <div className="flex items-center justify-center ">
               <PixIcon />
               <span className="text-primary ">
-               <p className="font-black text-xl">5% no Pix</p>  <p>Pagamento à vista</p> 
+                <p className="font-black text-xl">5% no Pix</p>
+                <p>Pagamento à vista</p>
               </span>
             </div>
           </section>
-        
-        
-         
-          <section className="col-span-full md:col-span-2 lg:col-span-3 flex justify-center md:justify-start">
-  <button className="bg-green-800 text-white mt-32 ml-12 p-2.5">
+
+          <div className="flex justify-center mb-8">
+            <Carrossel />
+          </div>
+          <section className="col-span-full md:col-span-2 lg:col-span-3 flex justify-center md:justify-start mt-20 mb-8">
+            <Link to="/produtos" className="bg-green-800 text-white p-2.5">
               Lançamentos
-            </button>
+            </Link>
           </section>
 
-          {/* Lista de Produtos */}
-          <div className="col-span-full flex-grow relative z-0">
+          <div className="col-span-full flex flex-wrap justify-center gap-6">
             <ListaProdutos />
           </div>
         </div>
