dfad7be77fa2f922a18a4b085f1b7705f2c6218f
devgabrielnascimento <devgabrielnascimento@gmail.com>
2024-06-27 04:25:45 -0300

    :lipstick: estilizando

--- src/components/produtos/buscaProdutos/BuscaProdutos.tsx
+++ src/components/produtos/buscaProdutos/BuscaProdutos.tsx
@@ -6,20 +6,33 @@ import { buscar } from "../../../service/Service";
 import CardProduto from "../cardProdutos/CardProdutos";
 import { toastAlerta } from "../../../utils/toastAlerta";
 import { useNavigate, useParams } from "react-router-dom";
+import FinderIcon from "../../../assets/FinderIcon.svg";
 
 function BuscaProduto() {
   const navigate = useNavigate();
   const { query } = useParams();
   const [produto, setProduto] = useState<Produto[]>([]);
-
+  const [loading, setLoading] = useState<boolean>(true); // Estado de loading
+  const [timerDone, setTimerDone] = useState<boolean>(false); // Estado do timer
   const { usuario, handleLogout } = useContext(AuthContext);
   const token = usuario.token;
 
   async function buscarProduto() {
-    if (usuario.nome == "") {
+    setLoading(true);
+    setTimerDone(false);
+
+    if (usuario.nome === "") {
       toastAlerta("Logue para usar a busca", "info");
       navigate("/login");
+      return;
     }
+
+    if (!query) {
+      toastAlerta("Por favor, insira um termo de busca", "info");
+      setLoading(false);
+      return;
+    }
+
     try {
       await buscar("/produtos/nome/" + query, setProduto, {
         headers: { Authorization: token },
@@ -29,15 +42,20 @@ function BuscaProduto() {
         toastAlerta("O token expirou, favor logar novamente", "info");
         handleLogout();
       }
+    } finally {
+      setLoading(false);
     }
   }
 
   useEffect(() => {
+    const timer = setTimeout(() => setTimerDone(true), 2200); // Timer de 3 segundos
     buscarProduto();
-  }, [produto.length]);
+    return () => clearTimeout(timer);
+  }, [query]);
+
   return (
     <>
-      {produto.length === 0 && (
+      {loading ? (
         <Triangle
           visible={true}
           height="130"
@@ -47,19 +65,33 @@ function BuscaProduto() {
           wrapperStyle={{}}
           wrapperClass="triangle-wrapper mx-auto"
         />
-      )}
-      <div className="flex flex-grow justify-center w-full my-4">
-        <div className="container flex flex-col">
-          <h2>Resultados da busca para: {query}</h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 content-center justify-center items-center">
-            {produto.map((produto) => (
+      ) : (
+        <div className="flex flex-grow justify-center w-full my-4">
+          <div className="container flex flex-col">
+            <h2>Resultados da busca para: {query}</h2>
+            {produto.length === 0 && timerDone ? (
+              <p>Nenhum resultado encontrado.</p>
+            ) : (
               <>
-                <CardProduto key={produto.id} produto={produto} />
+                {!timerDone && (
+                  <div className="grid justify-center flex-col items-center">
+                    <p className="text-primary font-semibold text-lg">Pesquisando...</p>
+                    <img src={FinderIcon} alt="" />
+                     {}
+                  </div>
+                )}
+                {timerDone && produto.length !== 0 && (
+                  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 content-center justify-center items-center">
+                    {produto.map((produto) => (
+                      <CardProduto key={produto.id} produto={produto} />
+                    ))}
+                  </div>
+                )}
               </>
-            ))}
+            )}
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
