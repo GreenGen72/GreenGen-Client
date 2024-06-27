46a79aae96343156950d8a66955541fd1951b79c
Gabriel Rodrigues <gabers357@gmail.com>
2024-06-26 22:39:14 -0300

    💄feat: Adicionar banners à tela inicial

--- src/pages/sobreNos/SobreNos.tsx
+++ src/pages/sobreNos/SobreNos.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import GithubIcon from '../../components/GithubIcon/GithubIcon';
+import GithubIcon from "../../components/GithubIcon/GithubIcon";
 import LinkedInIcon from "../../components/LinkedInIcon/LinkedInIcon";
 const AboutUs: React.FC = () => {
   return (
@@ -75,85 +75,150 @@ const AboutUs: React.FC = () => {
             </span>
           </p>
         </div>
-        <p className="text-2xl font-bold mb-4 text-center text-green-700">Nosso time</p>
-        <div className="flex gap-10
- justify-center items-center text-center">
+        <p className="text-2xl font-bold mb-4 text-center text-green-700">
+          Nosso time
+        </p>
+        <div
+          className="flex gap-10
+ justify-center items-center text-center"
+        >
           <div className="flex flex-col justify-center items-center text-center hoverSB">
-              <img
-                src="https://media.licdn.com/dms/image/D4D03AQE3Vb479FjfKg/profile-displayphoto-shrink_400_400/0/1679620505127?e=1721865600&v=beta&t=YjvF1yui4XmH8GZos9_FnWuj7qrU3K3S22RNWAdRaSA"
-                width="100px"
-                alt="Foto de Gabriel Rodrigues no GitHub"
-                className="rounded-full"
-              />
-              <p className="text-center mt-2">Gabriel Rodrigues</p>
-            <div className="flex gap-4"> <a href="https://github.com/ApenasGabs" target="_blank" className=""><GithubIcon/>
-              </a><a href="https://www.linkedin.com/in/apenasgabs/" target="_blank"><LinkedInIcon/></a></div> 
-             </div>
-          <div className="flex flex-col justify-center items-center text-center hoverSB">
-              <img
-                src="https://avatars.githubusercontent.com/u/99691142?v=4"
-                width="100px"
-                alt="Foto de Dorivania Minante no GitHub"
-                className="rounded-full"
-              />
-              <p className="text-center mt-2">Dorivania Minante</p>
-              <div className="flex gap-4"><a href="https://github.com/dori-minante " target="_blank"><GithubIcon/></a><a href="https://www.linkedin.com/in/dorivaniasm/" target="_blank"><LinkedInIcon/></a></div> 
-          </div>
-          <div className="flex flex-col justify-center items-center text-center hoverSB">
-            <a href="https://github.com/Jonathangaruti">
+            <img
+              src="https://media.licdn.com/dms/image/D4D03AQHFpp1_HjjM0g/profile-displayphoto-shrink_200_200/0/1719181975880?e=1724889600&v=beta&t=IgMmnJ6M7SvTL0lDrjOAmdicI9jeZ5-_jvBCTpp4T2k"
+              width="100px"
+              alt="Foto de Gabriel Rodrigues no GitHub"
+              className="rounded-full"
+            />
+            <p className="text-center mt-2">Gabriel Rodrigues</p>
+            <div className="flex gap-4">
+              {" "}
+              <a
+                href="https://github.com/ApenasGabs"
+                target="_blank"
+                className=""
+              >
+                <GithubIcon />
               </a>
-              <img
-                src="https://media.licdn.com/dms/image/D4D03AQHH_h-UVZHn3Q/profile-displayphoto-shrink_400_400/0/1715265331079?e=1721865600&v=beta&t=aahQBXgEw7FICeK1IjrRg6GIYmv4Ibp_ccIc6_ld1U0"
-                width="100px"
-                alt="Foto de Jonathan Garuti no GitHub"
-                className="rounded-full"
-              />
-              <p className="text-center mt-2">Jonathan Garuti</p>
-              <div className="flex gap-4"><a href="https://github.com/Jonathangaruti" target="_blank"><GithubIcon/></a><a href="https://www.linkedin.com/in/jonathangaruti/" target="_blank"><LinkedInIcon/></a></div> 
-          </div>
-          <div className="flex flex-col justify-center items-center text-center hoverSB">
-            <a href="https://github.com/NandayGB">
+              <a href="https://www.linkedin.com/in/apenasgabs/" target="_blank">
+                <LinkedInIcon />
               </a>
-              <img
-                src="https://avatars.githubusercontent.com/u/112911182?v=4"
-                width="100px"
-                alt="Foto de Fernanda Barbosa no GitHub"
-                className="rounded-full"
-              />
-              <p className="text-center mt-2">Fernanda Barbosa</p>
-              <div className="flex gap-4"><a href="https://github.com/NandayGB" target="_blank"><GithubIcon/></a><a href="https://www.linkedin.com/in/barbosaafernanda/" target="_blank"><LinkedInIcon/></a></div> 
+            </div>
           </div>
           <div className="flex flex-col justify-center items-center text-center hoverSB">
-              <img
-                src="https://avatars.githubusercontent.com/u/147649384?v=4"
-                width="100px"
-                alt="Foto de Ravi Brito no GitHub"
-                className="rounded-full"
-              />
-              <p className="text-center mt-2">Ravi Brito</p>
-              <div className="flex gap-4"><a href="https://github.com/RaviBrito" target="_blank">
-              <GithubIcon/></a><a href="https://www.linkedin.com/in/ravi-brito/" target="_blank"><LinkedInIcon/></a></div> 
+            <img
+              src="https://avatars.githubusercontent.com/u/99691142?v=4"
+              width="100px"
+              alt="Foto de Dorivania Minante no GitHub"
+              className="rounded-full"
+            />
+            <p className="text-center mt-2">Dorivania Minante</p>
+            <div className="flex gap-4">
+              <a href="https://github.com/dori-minante " target="_blank">
+                <GithubIcon />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/dorivaniasm/"
+                target="_blank"
+              >
+                <LinkedInIcon />
+              </a>
+            </div>
           </div>
           <div className="flex flex-col justify-center items-center text-center hoverSB">
-              <img
-                src="https://avatars.githubusercontent.com/u/157232091?v=4"
-                width="100px"
-                alt="Foto de Tainá Poppi no GitHub"
-                className="rounded-full"
-              />
-              <p className="text-center mt-2">Tainá Poppi</p>
-              <div className="flex gap-4"><a href="https://github.com/tainapoppi" target="_blank"><GithubIcon/></a><a href="https://www.linkedin.com/in/tainapoppi/" target="_blank"><LinkedInIcon/></a></div> 
+            <a href="https://github.com/Jonathangaruti"></a>
+            <img
+              src="https://media.licdn.com/dms/image/D4D03AQHH_h-UVZHn3Q/profile-displayphoto-shrink_400_400/0/1715265331079?e=1721865600&v=beta&t=aahQBXgEw7FICeK1IjrRg6GIYmv4Ibp_ccIc6_ld1U0"
+              width="100px"
+              alt="Foto de Jonathan Garuti no GitHub"
+              className="rounded-full"
+            />
+            <p className="text-center mt-2">Jonathan Garuti</p>
+            <div className="flex gap-4">
+              <a href="https://github.com/Jonathangaruti" target="_blank">
+                <GithubIcon />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/jonathangaruti/"
+                target="_blank"
+              >
+                <LinkedInIcon />
+              </a>
+            </div>
           </div>
           <div className="flex flex-col justify-center items-center text-center hoverSB">
-              <img
-                src="https://avatars.githubusercontent.com/u/92201977?v=4"
-                width="100px"
-                alt="Foto de Gabriel Nascimento no GitHub"
-                className="rounded-full"
-              />
-              <p className="text-center mt-2">Gabriel Nascimento</p>
-              <div className="flex gap-4"><a href="https://github.com/devgabrielnascimento" target="_blank"><GithubIcon/>
-              </a><a href="https://www.linkedin.com/in/devgabrielnascimento" target="_blank"><LinkedInIcon/></a></div> 
+            <a href="https://github.com/NandayGB"></a>
+            <img
+              src="https://avatars.githubusercontent.com/u/112911182?v=4"
+              width="100px"
+              alt="Foto de Fernanda Barbosa no GitHub"
+              className="rounded-full"
+            />
+            <p className="text-center mt-2">Fernanda Barbosa</p>
+            <div className="flex gap-4">
+              <a href="https://github.com/NandayGB" target="_blank">
+                <GithubIcon />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/barbosaafernanda/"
+                target="_blank"
+              >
+                <LinkedInIcon />
+              </a>
+            </div>
+          </div>
+          <div className="flex flex-col justify-center items-center text-center hoverSB">
+            <img
+              src="https://avatars.githubusercontent.com/u/147649384?v=4"
+              width="100px"
+              alt="Foto de Ravi Brito no GitHub"
+              className="rounded-full"
+            />
+            <p className="text-center mt-2">Ravi Brito</p>
+            <div className="flex gap-4">
+              <a href="https://github.com/RaviBrito" target="_blank">
+                <GithubIcon />
+              </a>
+              <a href="https://www.linkedin.com/in/ravi-brito/" target="_blank">
+                <LinkedInIcon />
+              </a>
+            </div>
+          </div>
+          <div className="flex flex-col justify-center items-center text-center hoverSB">
+            <img
+              src="https://avatars.githubusercontent.com/u/157232091?v=4"
+              width="100px"
+              alt="Foto de Tainá Poppi no GitHub"
+              className="rounded-full"
+            />
+            <p className="text-center mt-2">Tainá Poppi</p>
+            <div className="flex gap-4">
+              <a href="https://github.com/tainapoppi" target="_blank">
+                <GithubIcon />
+              </a>
+              <a href="https://www.linkedin.com/in/tainapoppi/" target="_blank">
+                <LinkedInIcon />
+              </a>
+            </div>
+          </div>
+          <div className="flex flex-col justify-center items-center text-center hoverSB">
+            <img
+              src="https://avatars.githubusercontent.com/u/92201977?v=4"
+              width="100px"
+              alt="Foto de Gabriel Nascimento no GitHub"
+              className="rounded-full"
+            />
+            <p className="text-center mt-2">Gabriel Nascimento</p>
+            <div className="flex gap-4">
+              <a href="https://github.com/devgabrielnascimento" target="_blank">
+                <GithubIcon />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/devgabrielnascimento"
+                target="_blank"
+              >
+                <LinkedInIcon />
+              </a>
+            </div>
           </div>
         </div>
       </div>
